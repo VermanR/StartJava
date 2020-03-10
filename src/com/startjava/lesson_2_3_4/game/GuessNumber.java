b49fa1b33215a64c903e79eebbdb5791f7444c4c
VermanR <ruslanverman@gmail.com>
2020-03-10 20:58:06 +0300

    rename folder

---
+++ src/com/startjava/lesson_2_3_4/game/GuessNumber.java
@@ -0,0 +1,44 @@
+package com.startjava.lesson_2_3_4.game;
+
+import java.lang.Math;
+import java.util.Scanner;
+
+public class GuessNumber {
+	private Player player1;
+	private Player player2;
+
+	Scanner sc = new Scanner(System.in);
+
+	public GuessNumber(Player player1, Player player2) {
+		this.player1 = player1;
+		this.player2 = player2;
+	}
+
+	public void guess() {
+		int computerNumber = (int) (Math.random() * 101);
+		int number;
+		do {
+			System.out.print(player1.getName() + ", enter a number: ");
+			player1.setNumber(sc.nextInt());
+			number = player1.getNumber();
+			if (number == computerNumber) {
+				System.out.println(player1.getName() + ", you guessed!");
+			} else  if (number < computerNumber) {
+				System.out.println(player1.getName() + ", you entered a number less than what the computer did");
+			} else if (number > computerNumber) {
+				System.out.println(player1.getName() + ", you entered a number greater than what the computer did");
+			}
+
+			System.out.print(player2.getName() + ", enter a number: ");
+			player2.setNumber(sc.nextInt());
+			number = player2.getNumber();
+			if (number == computerNumber) {
+				System.out.println(player2.getName() + ", you guessed!");
+			} else  if (number < computerNumber) {
+				System.out.println(player2.getName() + ", you entered a number less than what the computer did");
+			} else if (number > computerNumber) {
+				System.out.println(player2.getName() + ", you entered a number greater than what the computer did");
+			}
+		} while (number != computerNumber);
+	}	
+}
